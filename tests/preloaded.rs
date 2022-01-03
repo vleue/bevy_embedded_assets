474a7a816692b9e8dd2c31280d99f39e11783725
François <8672791+mockersf@users.noreply.github.com>
2022-01-03 23:55:59 +0100

    add test on build script

---
+++ tests/preloaded.rs
@@ -0,0 +1,15 @@
+use std::path::Path;
+
+use bevy_embedded_assets::EmbeddedAssetIo;
+
+#[test]
+fn check_preloaded() {
+    let embedded = EmbeddedAssetIo::preloaded();
+
+    assert!(embedded.load_path_sync(&Path::new("example_asset")).is_ok());
+    let raw_asset = embedded
+        .load_path_sync(&Path::new("example_asset"))
+        .unwrap();
+    assert!(String::from_utf8(raw_asset.clone()).is_ok());
+    assert_eq!(String::from_utf8(raw_asset).unwrap(), "hello");
+}
