721620d65ea451fe0f589fff83f9b594a3eb9a3c
François <8672791+mockersf@users.noreply.github.com>
2022-01-06 00:01:10 +0100

    added test with a fail with special chars

--- tests/preloaded.rs
+++ tests/preloaded.rs
@@ -12,4 +12,9 @@ fn check_preloaded() {
         .unwrap();
     assert!(String::from_utf8(raw_asset.clone()).is_ok());
     assert_eq!(String::from_utf8(raw_asset).unwrap(), "hello");
+
+    assert!(embedded.load_path_sync(&Path::new("açèt")).is_ok());
+    let raw_asset = embedded.load_path_sync(&Path::new("açèt")).unwrap();
+    assert!(String::from_utf8(raw_asset.clone()).is_ok());
+    assert_eq!(String::from_utf8(raw_asset).unwrap(), "with special chars");
 }
