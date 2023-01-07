4b88523630afcf80f100d39f297e3478357a9fd6
François <mockersf@gmail.com>
2023-01-07 16:31:20 +0100

    spit tests

--- tests/preloaded.rs
+++ tests/preloaded.rs
@@ -3,18 +3,27 @@ use std::path::Path;
 use bevy_embedded_assets::EmbeddedAssetIo;
 
 #[test]
-fn check_preloaded() {
+fn check_preloaded_simple() {
     let embedded = EmbeddedAssetIo::preloaded();
 
-    assert!(embedded.load_path_sync(&Path::new("example_asset")).is_ok());
-    let raw_asset = embedded
-        .load_path_sync(&Path::new("example_asset"))
-        .unwrap();
+    let path = "example_asset";
+
+    let loaded = embedded.load_path_sync(&Path::new(path));
+    assert!(loaded.is_ok());
+    let raw_asset = loaded.unwrap();
     assert!(String::from_utf8(raw_asset.clone()).is_ok());
     assert_eq!(String::from_utf8(raw_asset).unwrap(), "hello");
+}
 
-    assert!(embedded.load_path_sync(&Path::new("açèt")).is_ok());
-    let raw_asset = embedded.load_path_sync(&Path::new("açèt")).unwrap();
+#[test]
+fn check_preloaded_special_chars() {
+    let embedded = EmbeddedAssetIo::preloaded();
+
+    let path = "açèt";
+
+    let loaded = embedded.load_path_sync(&Path::new(path));
+    assert!(loaded.is_ok());
+    let raw_asset = loaded.unwrap();
     assert!(String::from_utf8(raw_asset.clone()).is_ok());
     assert_eq!(String::from_utf8(raw_asset).unwrap(), "with special chars");
 }
